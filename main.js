0bad471aaaa870d4efbe1c76e3a826e4cbf15415
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2018-07-21 03:34:39 +0500

    Showing player and apple on the screen

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
 
-// let gameCanvas, screen;
-// const FPS = 10;
+let gameCanvas, screen;
+const FPS = 10;
+// const tilesCount = 20;
+// let playerPosX = 10, playerPosY = 10;
+// let applePosX = 15, applePosY = 15;
 
 window.onload = function () {
     // get canvas object from DOM
-    // gameCanvas = document.getElementById('gameCanvas');
+    gameCanvas = document.getElementById('gameCanvas');
     // get canvas context (so we can use it for painting elements inside)
-    // screen = gameCanvas.getContext('2d');
+    screen = gameCanvas.getContext('2d');
 
     // run the game loop
-    // setInterval(() => {
-    //     gameLoop();
-    // }, 1000 / FPS);
+    setInterval(() => {
+        gameLoop();
+    }, 1000 / FPS);
 }
 
 function gameLoop() {
     // game screen background
-    // screen.fillStyle = '#000';
-    // screen.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+    screen.fillStyle = '#000';
+    screen.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+    // showing snake
+    // screen.fillStyle = 'yellow';
+    // screen.fillRect(playerPosX * tilesCount, playerPosY * tilesCount ,tilesCount - 2, tilesCount - 2);
+
+
+    // showing apple
+    // screen.fillStyle = "red";
+    // screen.fillRect(applePosX * tilesCount, applePosY * tilesCount, tilesCount - 2, tilesCount - 2);
 }
