50ca87d56d42c08467a2638d3b4c466f4ced38f8
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2018-07-21 03:42:33 +0500

    letting the snake move on its own and we just change the direction

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const FPS = 10;
 const tilesCount = 20;
 let playerPosX = 10, playerPosY = 10;
 let applePosX = 15, applePosY = 15;
+// let xVelocity = 0, yVelocity = 0;
 
 window.onload = function () {
     // get canvas object from DOM
@@ -12,7 +13,7 @@ window.onload = function () {
     screen = gameCanvas.getContext('2d');
 
     // add keyboard event listener
-    // document.addEventListener('keyup', onKeyUp);
+    document.addEventListener('keyup', onKeyUp);
     
     // run the game loop
     setInterval(() => {
@@ -21,6 +22,10 @@ window.onload = function () {
 }
 
 function gameLoop() {
+
+    // playerPosX += xVelocity;
+    // playerPosY += yVelocity;
+
     // game screen background
     screen.fillStyle = '#000';
     screen.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
@@ -35,19 +40,23 @@ function gameLoop() {
     screen.fillRect(applePosX * tilesCount, applePosY * tilesCount, tilesCount - 2, tilesCount - 2);
 }
 
-// function onKeyUp(event) {
-//     switch(event.keyCode) {
-//         case 37:    // left
-//             playerPosX--;
-//             break;
-//         case 38:    // up
-//             playerPosY--;
-//             break;
-//         case 39:    // right
-//             playerPosX++;
-//             break;
-//         case 40:    // down
-//             playerPosY++;
-//             break;
-//     }
-// }
+function onKeyUp(event) {
+    // switch(event.keyCode) {
+    //     case 37:    // left
+    //         xVelocity = -1;      // go left
+    //         yVelocity = 0;      // no Y axis movement
+    //         break;
+    //     case 38:    // up
+    //         yVelocity = -1;     // go up
+    //         xVelocity = 0;      // no X axis movement
+    //         break;
+    //     case 39:    // right
+    //         xVelocity = 1;      // go right
+    //         yVelocity = 0;      // no Y axis movement
+    //         break;
+    //     case 40:    // down
+    //         yVelocity = 1;      // go down
+    //         xVelocity = 0;      // no X axis movement
+    //         break;
+    // }
+}
