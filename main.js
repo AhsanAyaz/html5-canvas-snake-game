b85c6cf4d7cf6769ba5f758cdbdbaf8d62c82f12
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2018-07-21 03:32:36 +0500

    initial commit. game screen setup

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// let gameCanvas, screen;
+// const FPS = 10;
+
+window.onload = function () {
+    // get canvas object from DOM
+    // gameCanvas = document.getElementById('gameCanvas');
+    // get canvas context (so we can use it for painting elements inside)
+    // screen = gameCanvas.getContext('2d');
+
+    // run the game loop
+    // setInterval(() => {
+    //     gameLoop();
+    // }, 1000 / FPS);
+}
+
+function gameLoop() {
+    // game screen background
+    // screen.fillStyle = '#000';
+    // screen.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+}
